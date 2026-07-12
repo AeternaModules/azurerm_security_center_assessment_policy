8d1c5d6cd3d503519f9ad35408d9111657fd0131
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:06 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,41 +1,37 @@
-output "security_center_assessment_policies" {
-  description = "All security_center_assessment_policy resources"
-  value       = azurerm_security_center_assessment_policy.security_center_assessment_policies
-}
 output "security_center_assessment_policies_categories" {
-  description = "List of categories values across all security_center_assessment_policies"
-  value       = [for k, v in azurerm_security_center_assessment_policy.security_center_assessment_policies : v.categories]
+  description = "Map of categories values across all security_center_assessment_policies, keyed the same as var.security_center_assessment_policies"
+  value       = { for k, v in azurerm_security_center_assessment_policy.security_center_assessment_policies : k => v.categories }
 }
 output "security_center_assessment_policies_description" {
-  description = "List of description values across all security_center_assessment_policies"
-  value       = [for k, v in azurerm_security_center_assessment_policy.security_center_assessment_policies : v.description]
+  description = "Map of description values across all security_center_assessment_policies, keyed the same as var.security_center_assessment_policies"
+  value       = { for k, v in azurerm_security_center_assessment_policy.security_center_assessment_policies : k => v.description }
 }
 output "security_center_assessment_policies_display_name" {
-  description = "List of display_name values across all security_center_assessment_policies"
-  value       = [for k, v in azurerm_security_center_assessment_policy.security_center_assessment_policies : v.display_name]
+  description = "Map of display_name values across all security_center_assessment_policies, keyed the same as var.security_center_assessment_policies"
+  value       = { for k, v in azurerm_security_center_assessment_policy.security_center_assessment_policies : k => v.display_name }
 }
 output "security_center_assessment_policies_implementation_effort" {
-  description = "List of implementation_effort values across all security_center_assessment_policies"
-  value       = [for k, v in azurerm_security_center_assessment_policy.security_center_assessment_policies : v.implementation_effort]
+  description = "Map of implementation_effort values across all security_center_assessment_policies, keyed the same as var.security_center_assessment_policies"
+  value       = { for k, v in azurerm_security_center_assessment_policy.security_center_assessment_policies : k => v.implementation_effort }
 }
 output "security_center_assessment_policies_name" {
-  description = "List of name values across all security_center_assessment_policies"
-  value       = [for k, v in azurerm_security_center_assessment_policy.security_center_assessment_policies : v.name]
+  description = "Map of name values across all security_center_assessment_policies, keyed the same as var.security_center_assessment_policies"
+  value       = { for k, v in azurerm_security_center_assessment_policy.security_center_assessment_policies : k => v.name }
 }
 output "security_center_assessment_policies_remediation_description" {
-  description = "List of remediation_description values across all security_center_assessment_policies"
-  value       = [for k, v in azurerm_security_center_assessment_policy.security_center_assessment_policies : v.remediation_description]
+  description = "Map of remediation_description values across all security_center_assessment_policies, keyed the same as var.security_center_assessment_policies"
+  value       = { for k, v in azurerm_security_center_assessment_policy.security_center_assessment_policies : k => v.remediation_description }
 }
 output "security_center_assessment_policies_severity" {
-  description = "List of severity values across all security_center_assessment_policies"
-  value       = [for k, v in azurerm_security_center_assessment_policy.security_center_assessment_policies : v.severity]
+  description = "Map of severity values across all security_center_assessment_policies, keyed the same as var.security_center_assessment_policies"
+  value       = { for k, v in azurerm_security_center_assessment_policy.security_center_assessment_policies : k => v.severity }
 }
 output "security_center_assessment_policies_threats" {
-  description = "List of threats values across all security_center_assessment_policies"
-  value       = [for k, v in azurerm_security_center_assessment_policy.security_center_assessment_policies : v.threats]
+  description = "Map of threats values across all security_center_assessment_policies, keyed the same as var.security_center_assessment_policies"
+  value       = { for k, v in azurerm_security_center_assessment_policy.security_center_assessment_policies : k => v.threats }
 }
 output "security_center_assessment_policies_user_impact" {
-  description = "List of user_impact values across all security_center_assessment_policies"
-  value       = [for k, v in azurerm_security_center_assessment_policy.security_center_assessment_policies : v.user_impact]
+  description = "Map of user_impact values across all security_center_assessment_policies, keyed the same as var.security_center_assessment_policies"
+  value       = { for k, v in azurerm_security_center_assessment_policy.security_center_assessment_policies : k => v.user_impact }
 }
 
